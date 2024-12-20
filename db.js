11f65c5c53740f05e2b03b6a3267368c9e0ea6fb
Bharat Kumar_155 <kumarbharat99928@gmail.com>
2024-12-20 08:19:23 +0530

    backed to IPv4 address for mongodb

--- db.js
+++ db.js
@@ -1,10 +1,10 @@
 const mongoose = require("mongoose");
 
-const mongURL = "mongodb://localhost:27017/first_db";
+const mongURL = "mongodb://127.0.0.1:27017/first_db";
 const mongoconnection = async () => {
     try {
         await mongoose.connect(mongURL);
-        console.log("Mongo db connected");
+        console.log("MongoDB connected successfully");
     } catch (error) {
         console.error("Mongoose connection error:", error);
     }
